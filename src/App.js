f4714526946c88d6a601414e98d8fddff9bb07d4
Gyinae <dev.gyinae@gmail.com>
2023-07-03 19:44:12 +0000

    navbar commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Header from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
